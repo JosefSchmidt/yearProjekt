eface6daafb5cbb82baa9d8974558abbced700de
Josef Brøndum Schmidt <yoss0002@stud.kea.dk>
2018-05-08 14:20:22 +0200

    Spring Security basic implementeret

---
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -0,0 +1,50 @@
+package com.example.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Qualifier("dataSource")
+    @Autowired
+    DataSource dataSource;
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select username,password, enabled from user where username=?")
+                .authoritiesByUsernameQuery("select username, authority from authorities where username=?");
+    }
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+
+        http.authorizeRequests().antMatchers("/admin/*").hasRole("ADMIN")
+                .antMatchers("/user/*").hasRole("USER")
+                .antMatchers("/newUser").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .permitAll();
+
+
+    }
+}
