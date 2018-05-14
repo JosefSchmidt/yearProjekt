7d814618f350721d3311f7ca11a4844f4554bce9
Josef Brøndum Schmidt <yoss0002@stud.kea.dk>
2018-05-14 13:55:23 +0200

    employee tilføjet og view i home og home_admin

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,9 +1,39 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Employee;
+import com.example.demo.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EmployeeController {
+
+    @Autowired
+    EmployeeService employeeService;
+
+
+    @GetMapping(value = "/add_employee")
+    public String add_employee(Model model){
+
+        model.addAttribute("employee", new Employee());
+
+        return "add_employee";
+    }
+
+    @PostMapping(value = "/add_employee")
+    public String add_employee(@RequestParam String name, @RequestParam String position, @RequestParam int provision_goal){
+
+        employeeService.addEmployee(name, position, provision_goal);
+
+        return "redirect:/home_admin";
+    }
+
+
+
 
 
 }
---
+++ src/main/java/com/example/demo/services/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    void addEmployee(String name, String position, int provision_goal);
+
+    List viewEmployee();
+
+
+
+}
